b9e1212af7098967548fa0f6e084219f4e71518d
Bikash-Tri <131143131+Bikash-Bijliride@users.noreply.github.com>
2024-11-10 11:31:10 +0530

    Continuin project

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+// const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+
+//const testRoutes = require("./routes/testRoutes");
+const authRoutes = require("./routes/authRoutes");
+const inventoryRoutes = require("./routes/inventoryRoutes");
+// const analyticsRoutes = require("./routes/analyticsRoutes");
+// const adminRoutes = require("./routes/adminRoutes");
+
+
+
+dotenv.config({});
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+// api's
+//app.use("/api/v1/test", testRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/inventory", inventoryRoutes);
+// app.use("/api/v1/analytics", analyticsRoutes);
+// app.use("/api/v1/admin", adminRoutes);
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
